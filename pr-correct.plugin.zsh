4dbbf6443ec0a54102fcb8b75f5605fadc66068e
Grigorii Horos <horosgrisa@gmail.com>
2020-04-25 13:17:46 +0300

    Fix message

--- pr-correct.plugin.zsh
+++ pr-correct.plugin.zsh
@@ -1,3 +1,11 @@
 #!/usr/bin/env zsh
 
-typeset -g pr_correct="${c[cyan]} Change ${c[red]}%R${c[cyan]} to ${c[green]}%r${c[cyan]}? (${c[green]}${c[underline]}Y${c[reset]}${c[green]}es${c[cyan]}, ${c[red]}${c[underline]}N${c[reset]}${c[red]}o${c[cyan]}, ${c[red]}${c[underline]}A${c[reset]}${c[red]}bort${c[cyan]}, ${c[yellow]}${c[underline]}E${c[reset]}${c[yellow]}dit${c[cyan]}) ${c[blue]}> ${c[reset]}"
+typeset -g pr_correct
+pr_correct+="${c[cyan]} Change ${c[red]}${c[bold]}%R${c[reset]}${c[cyan]} to ${c[green]}${c[bold]}%r${c[reset]}${c[cyan]}? "
+pr_correct+='('
+pr_correct+="${c[green]}${c[underline]}Y${c[reset]}${c[green]}es${c[cyan]}, "
+pr_correct+="${c[red]}${c[underline]}N${c[reset]}${c[red]}o${c[cyan]}, "
+pr_correct+="${c[red]}${c[underline]}A${c[reset]}${c[red]}bort${c[cyan]}, "
+pr_correct+="${c[yellow]}${c[underline]}E${c[reset]}${c[yellow]}dit${c[cyan]}"
+pr_correct+=') '
+pr_correct+="${c[blue]}> ${c[reset]}"
