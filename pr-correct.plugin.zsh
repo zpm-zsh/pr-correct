a576fd4b47f641ad0c4dd81cb9d3fb74ed9d46fc
Tom Prince <tom.prince@hocat.ca>
2021-04-18 00:54:49 -0600

    Gracefully handle being loaded twice.

--- pr-correct.plugin.zsh
+++ pr-correct.plugin.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 typeset -g pr_correct
-pr_correct+="${c[cyan]} Change ${c[red]}${c[bold]}%R${c[reset]}${c[cyan]} to ${c[green]}${c[bold]}%r${c[reset]}${c[cyan]}? "
+pr_correct="${c[cyan]} Change ${c[red]}${c[bold]}%R${c[reset]}${c[cyan]} to ${c[green]}${c[bold]}%r${c[reset]}${c[cyan]}? "
 pr_correct+='('
 pr_correct+="${c[green]}${c[underline]}Y${c[reset]}${c[green]}es${c[cyan]}, "
 pr_correct+="${c[red]}${c[underline]}N${c[reset]}${c[red]}o${c[cyan]}, "
